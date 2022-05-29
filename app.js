26bf3f02846ad96107a4b0a6104c1f58d94f7581
Klindzhov Islam <klindzhov@mail.ru>
2022-05-29 22:10:13 +0300

    Реализация логирования запросов и ошибок

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const errorHandler = require('./middlewares/error');
 const { login, createUser } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -15,6 +16,8 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   // useCreateIndex: true,
   // useFindAndModify: false,
 });
+
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -35,6 +38,8 @@ app.use('/users', require('./routes/users'));
 app.use('/movies', require('./routes/movies'));
 
 app.use((req, res, next) => next(new NotFoundError('Страница не найдена')));
+
+app.use(errorLogger);
 
 app.use(errorHandler);
 
