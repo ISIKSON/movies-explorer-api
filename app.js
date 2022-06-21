935bdfc45aa6458de0340806c29be5143c48e66f
Klindzhov Islam <klindzhov@mail.ru>
2022-06-21 19:36:22 +0300

    Подключение CORS

--- app.js
+++ app.js
@@ -13,12 +13,40 @@ const { PORT = 3000, NODE_ENV, DB_ADDRESS } = process.env;
 
 const app = express();
 
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+  'http://localhost:3000',
+  'https://mesto-islam.students.nomoredomains.xyz',
+  'http://mesto-islam.students.nomoredomains.xyz',
+];
+
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 mongoose.connect(NODE_ENV === 'production' ? DB_ADDRESS : 'mongodb://localhost:27017/nofilmsdb', {
   useNewUrlParser: true,
   // useCreateIndex: true,
   // useFindAndModify: false,
+});
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  console.log(origin);
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+  return null;
 });
 
 app.use(requestLogger);
